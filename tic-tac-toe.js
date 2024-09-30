cfd8133fbd80f5182b7687faa55944e079d0a552
esko <esko.voobus@protonmail.com>
2024-09-30 17:17:09 +0300

    feat: represent 3x3 grid with 2D array

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,16 @@
-console.log("Hello there!");
+const gameBoard = (function () {
+	const cols = 3;
+	const rows = 3;
+	const board = [];
+
+	for (let row = 0; row < rows; row++) {
+		board[row] = [];
+		for (let col = 0; col < cols; col++) {
+			board[row].push([]);
+		}
+	}
+
+	const getBoard = () => board;
+
+	return { getBoard };
+})();

de649a33ed23b136afe41231317af43d09c1cc32
esko <esko.voobus@protonmail.com>
2024-10-03 16:23:36 +0300

    feat: check for winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,12 +6,40 @@ function Board() {
 	];
 
 	const getBoard = () => board;
-	const isEmpty = (x, y) => board[x][y] === 0;
-	const markCell = (x, y, value) => {
-		board[x][y] = value;
+	const isEmpty = (row, col) => board[row][col] === 0;
+	const markCell = (row, col, id) => {
+		board[row][col] = id;
+	};
+	const checkWinner = (row, col, playerId) => {
+		if (checkRow(row, playerId)) return true;
+		if (checkCol(col, playerId)) return true;
+		if (checkDiag(playerId)) return true;
+
+		return false;
 	};
 
-	return { getBoard, markCell, isEmpty };
+	const checkRow = (row, id) => {
+		for (let col = 0; col < 3; col++) {
+			if (board[row][col] !== id) return false;
+		}
+		return true;
+	};
+
+	const checkCol = (col, id) => {
+		for (let row = 0; row < 3; row++) {
+			if (board[row][col] !== id) return false;
+		}
+		return true;
+	};
+
+	const checkDiag = (id) => {
+		for (let i = 0; i < 3; i++) {
+			if (board[i][i] !== id) return false;
+		}
+		return true;
+	};
+
+	return { getBoard, markCell, isEmpty, checkWinner };
 }
 
 
